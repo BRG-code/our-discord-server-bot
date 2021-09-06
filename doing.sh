50b820ae3749648be8937f3e81aa2be28b75e77d
cocoblue <cocoblue@kakao.com>
2021-09-06 16:48:42 +0900

    Test the Script

--- doing.sh
+++ doing.sh
@@ -2,7 +2,7 @@ NOWHOUR=$(date +%H)
 NOWWEEKDAY=$(date +%u)
 
 # 오전이면 명령 실행
-if [ "$NOWHOUR" -lt 12 ]
+if [ "$NOWHOUR" -lt 24 ]
 then
   echo -n "${WEBHOOK_LIST}" | base64 --decode > ./webhook_list.txt
   python3 main.py
